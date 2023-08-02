84791d6fb20a8d6563f65cb09ce38c9e9a573623
itian <aksdn1513@gmail.com>
2023-08-02 14:50:00 +0900

    이것저것 추가해씀

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,21 @@
+
 export default function Cart() {
+  let 장바구니 = ['Tomato','Pasta']
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem item={장바구니[0]}/>
+      <CartItem item={장바구니[1]}/>
     </div>
   )
 } 
+
+function CartItem(props){
+  return (
+    <div className="cart-item">
+        <p>{props.item}</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+  )
+}
