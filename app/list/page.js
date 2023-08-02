0269f82f1101efe4ca58113e428e5b13dd1a3c76
itian <aksdn1513@gmail.com>
2023-08-02 14:50:52 +0900

    이것저것

--- app/list/page.js
+++ app/list/page.js
@@ -1,7 +1,11 @@
+'use client';
+
+import { useState } from "react";
 
 export default function List() {
-
+  const [count,setCount] = useState([0,0,0]);
   let 상품들 = ['Tomatoes','pasta','Coconut'];
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -10,6 +14,17 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`food${i}.png`} className="food-img" />
             <h4>{상품} $40</h4>
+            <button onClick={()=>{
+              const copy = [...count]
+              copy[i]--;
+              setCount(copy)   
+            }}>-</button>
+            <span>{count[i]}</span>
+            <button onClick={()=>{
+              const copy = [...count]
+              copy[i]++;
+              setCount(copy)   
+            }}>+</button>
           </div>
         )
       })}
